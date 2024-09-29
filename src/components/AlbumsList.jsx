307aabcc10956335cfda119d6acf65b7df5788fd
Shariif-Rajpoot <mr8781256@gmail.com>
2024-09-28 19:28:59 -0700

    Adding redux toolkit query with tag system  for managing and adding data related to albums for each user

--- src/components/AlbumsList.jsx
+++ src/components/AlbumsList.jsx
@@ -1,7 +1,40 @@
 // import React from "react";
-
+import { useFetchAlbumsQuery, useAddAlbumsMutation } from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
 const AlbumsList = ({ user }) => {
-  return <div>Albums for {user.name}</div>;
-};
+  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  const [addAlbum, results] = useAddAlbumsMutation();
+  // console.log(data, error, isLoading);
+  const handleAddAlbum = () => {
+    addAlbum(user);
+  };
 
+  let content;
+  if (isLoading) content = <Skeleton times={4} className="w-full h-4" />;
+  else if (error) {
+    content = <div>Error loading albums</div>;
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          List of photos in the albums.
+        </ExpandablePanel>
+      );
+    });
+  }
+  return (
+    <div>
+      <div className="m-2 flex flex-row items-center justify-between">
+        <h3 className="text-lg font-bold">Albums for {user.name}</h3>
+        <Button onClick={handleAddAlbum} loading={results.isLoading} secondary>
+          + Add Album
+        </Button>
+      </div>
+      <div>{content}</div>
+    </div>
+  );
+};
 export default AlbumsList;
